3f2daa05844430d5f55bf63da782ef8d0fa02368
Nico122803 <nicoadalim@gmail.com>
2024-11-10 21:46:20 +0800

    Add project files.

---
+++ CalculatorApplications/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace CalculatorApplications
+{
+    partial class FrmCalculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxInput1 = new System.Windows.Forms.TextBox();
+            this.txtBoxInput2 = new System.Windows.Forms.TextBox();
+            this.cbOperator = new System.Windows.Forms.ComboBox();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblDisplayTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBoxInput1
+            // 
+            this.txtBoxInput1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtBoxInput1.Location = new System.Drawing.Point(170, 10);
+            this.txtBoxInput1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBoxInput1.Name = "txtBoxInput1";
+            this.txtBoxInput1.Size = new System.Drawing.Size(215, 34);
+            this.txtBoxInput1.TabIndex = 0;
+            // 
+            // txtBoxInput2
+            // 
+            this.txtBoxInput2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtBoxInput2.Location = new System.Drawing.Point(170, 69);
+            this.txtBoxInput2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBoxInput2.Name = "txtBoxInput2";
+            this.txtBoxInput2.Size = new System.Drawing.Size(215, 34);
+            this.txtBoxInput2.TabIndex = 1;
+            // 
+            // cbOperator
+            // 
+            this.cbOperator.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbOperator.FormattingEnabled = true;
+            this.cbOperator.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cbOperator.Location = new System.Drawing.Point(254, 42);
+            this.cbOperator.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbOperator.Name = "cbOperator";
+            this.cbOperator.Size = new System.Drawing.Size(54, 31);
+            this.cbOperator.TabIndex = 2;
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Font = new System.Drawing.Font("Consolas", 12F);
+            this.btnEqual.Location = new System.Drawing.Point(214, 133);
+            this.btnEqual.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(119, 33);
+            this.btnEqual.TabIndex = 4;
+            this.btnEqual.Text = "Calculate";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label1.Location = new System.Drawing.Point(30, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 28);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter First Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label2.Location = new System.Drawing.Point(9, 71);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 28);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter Second Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label3.Location = new System.Drawing.Point(106, 101);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 28);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Answer:";
+            // 
+            // lblDisplayTotal
+            // 
+            this.lblDisplayTotal.AutoSize = true;
+            this.lblDisplayTotal.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Underline);
+            this.lblDisplayTotal.Location = new System.Drawing.Point(170, 106);
+            this.lblDisplayTotal.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDisplayTotal.Name = "lblDisplayTotal";
+            this.lblDisplayTotal.Size = new System.Drawing.Size(292, 25);
+            this.lblDisplayTotal.TabIndex = 9;
+            this.lblDisplayTotal.Text = "___________________________________";
+            this.lblDisplayTotal.Click += new System.EventHandler(this.lblDisplayTotal_Click);
+            // 
+            // FrmCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(392, 262);
+            this.Controls.Add(this.lblDisplayTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.cbOperator);
+            this.Controls.Add(this.txtBoxInput2);
+            this.Controls.Add(this.txtBoxInput1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FrmCalculator";
+            this.Text = "CALCULATOR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtBoxInput1;
+        private TextBox txtBoxInput2;
+        private ComboBox cbOperator;
+        private Button btnEqual;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label lblDisplayTotal;
+    }
+}
